1ac598a13b5b499566d1ce6d4acaae074efe1d33
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-01-27 10:37:44 +0100

    first routing

--- maps/views.py
+++ maps/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+
 # Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = "maps/index.html"

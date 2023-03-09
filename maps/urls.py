6f174c58df5d841d3cd21f5e809f2cf6b4305016
Philippe Lemaire <philippe.lemaire@protonmail.com>
2023-03-09 11:15:06 +0100

    added some explainations with images on the index page

--- maps/urls.py
+++ maps/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = "maps"
@@ -7,4 +8,4 @@ app_name = "maps"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("display_map", views.display_map, name="display_map"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
